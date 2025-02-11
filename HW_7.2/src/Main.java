5e661b60ef7f63611c3a0aec100c2a038ada1298
Grigoriy <grig_baz@mail.ru>
2025-02-11 19:53:24 +0300

    add: added HW_7.2

---
+++ HW_7.2/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int n = 0;
+        while (n < 100) {
+            n += 2;
+            System.out.println(n);
+        }
+    }
+}
