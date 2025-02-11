b70526551c1e56713a8042b741d34a8af7c97988
Grigoriy <grig_baz@mail.ru>
2025-02-11 17:27:33 +0300

    ref: using ternary operators instead of if-else

--- HW_6.2/src/Main.java
+++ HW_6.2/src/Main.java
@@ -17,25 +17,29 @@ public class Main {
         double average = 0;
         double smallest = 0;
 
-        if (first > second && first > third) {
-            greatest = first;
-        }
-        else if (second > first && second > third) {
-            greatest = second;
-        }
-        else {
-            greatest = third;
-        }
+        greatest = (first > second && first > third) ? first : (second > first && second > third) ? second : third;
 
-        if (first < second && first < third) {
-            smallest = first;
-        }
-        else if (second < first && second < third) {
-            smallest = second;
-        }
-        else {
-            smallest = third;
-        }
+        smallest = (first < second && first < third) ? first : (second < first && second < third) ? second : third;
+
+//        if (first > second && first > third) {
+//            greatest = first;
+//        }
+//        else if (second > first && second > third) {
+//            greatest = second;
+//        }
+//        else {
+//            greatest = third;
+//        }
+//
+//        if (first < second && first < third) {
+//            smallest = first;
+//        }
+//        else if (second < first && second < third) {
+//            smallest = second;
+//        }
+//        else {
+//            smallest = third;
+//        }
 
         if ((first < second && first > third) || (first > second && first < third)) {
             average = first;
