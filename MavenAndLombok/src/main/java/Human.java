d9e8e002871ea7d076c1675d4de91046b7599e57
Grigoriy <grig_baz@mail.ru>
2025-06-23 17:14:15 +0300

    add: added Maven and Lombok practice

---
+++ MavenAndLombok/src/main/java/Human.java
@@ -0,0 +1,12 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Human {
+    private String name;
+    private int age;
+    private String gender;
+}
---
+++ MavenAndLombok/src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Human h1 = new Human();
+        Human h2 = new Human("H2", 21, "m");
+        System.out.println(h2.toString());
+    }
+}
